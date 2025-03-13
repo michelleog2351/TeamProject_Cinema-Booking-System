85521bb04196c0ca71b60456e7339277bf84f734
Michelle Ogunade <132501606+michelleog2351@users.noreply.github.com>
2025-03-13 20:51:17 +0000

    sort film names in alphabetical order, bugfix to times

--- public/app.js
+++ public/app.js
@@ -8,6 +8,11 @@ $(document).ready(function () {
 
 function filmDD() {
   $.getJSON("http://localhost:3000/films", function (data) {
+    // sorts the film names in alphabetical order
+    data.sort(function (a, b) {
+      return a.Name.localeCompare(b.Name);
+    });
+
     $("#selectFilm").html('<option value="" selected>Select Film</option>');
 
     $.each(data, function (i, film) {
@@ -40,27 +45,45 @@ function filmDD() {
 
 function dateDD() {
   $.getJSON("http://localhost:3000/screenings", function (data) {
+    //const today = new Date().toISOString().split("T")[0]; // Get today's date
+
     $("#selectDate").html(
       '<option value="screenings" selected>Select Date</option>'
     );
 
     $.each(data, function (i, screening) {
       let date = new Date(screening.Date).toISOString().split("T")[0];
+      // if (date >= today) {
+      // Only show today or future dates
       $("#selectDate").append(`<option value="${date}">${date}</option>`);
+      //}
     });
   });
 }
 
 function startTimeDD() {
+  const now = new Date();
+  const currentTime = now.toTimeString().split(" ")[0]; // Current time in HH:MM:SS
+
   $.getJSON("http://localhost:3000/startTimes", function (data) {
     $("#selectStartTime").html(
       '<option value="" selected>Select Time</option>'
     );
 
     $.each(data, function (i, screening) {
-      $("#selectStartTime").append(
-        `<option value="${screening.StartTime}">${screening.StartTime}</option>`
-      );
+      // Only show times later than the current time for today
+      if (screening.Date === now.toISOString().split("T")[0]) {
+        if (screening.StartTime > currentTime) {
+          $("#selectStartTime").append(
+            `<option value="${screening.StartTime}">${screening.StartTime}</option>`
+          );
+        }
+      } else {
+        // For future dates, show all times
+        $("#selectStartTime").append(
+          `<option value="${screening.StartTime}">${screening.StartTime}</option>`
+        );
+      }
     });
   });
 }
@@ -68,6 +91,6 @@ function startTimeDD() {
 function scrollToTop() {
   window.scrollTo({
     top: 0,
-    behavior: "smooth",
+    behavior: "smooth"
   });
 }
