f93fbf578b4f1fcbc709a03f2db8235214de63e6
Conor Callaghan <157692610+Conor153@users.noreply.github.com>
2025-03-16 16:45:35 +0000

    BugFix
    Changes made to sql file for remainingSeats

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.static("public"));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 //ADMIN ROUTES
@@ -55,20 +55,20 @@ app.get("/films", function (req, res) {
   film.getFilms(req, res);
 });
 
-app.get("/ageRatings", function(req,res){
-    film.getAgeRatings(req, res);
+app.get("/ageRatings", function (req, res) {
+  film.getAgeRatings(req, res);
 });
 
-app.get("/runningMinutes", function(req,res){
-    film.getRunningMinutes(req, res);
+app.get("/runningMinutes", function (req, res) {
+  film.getRunningMinutes(req, res);
 });
 
-app.get("/category", function(req,res){
-    film.getFilmCategories(req, res);
+app.get("/category", function (req, res) {
+  film.getFilmCategories(req, res);
 });
 
-app.get("/film/:filmID", function(req,res){
-	film.getFilm(req,res);
+app.get("/film/:filmID", function (req, res) {
+  film.getFilm(req, res);
 });
 
 app.post("/updateFilm/:filmID", function (req, res) {
@@ -91,48 +91,52 @@ app.post("/deleteFilm/:filmID", function (req, res) {
 
 ////////////////////////////////////////////////////////////
 
-  app.get("/screening/:screeningID", function(req,res){
-	screening.getScreening(req,res);
+app.get("/screening/:screeningID", function (req, res) {
+  screening.getScreening(req, res);
 });
 
-app.get("/screenings", function(req,res){
-	screening.getScreenings(req,res);
+app.get("/screenings", function (req, res) {
+  screening.getScreenings(req, res);
 });
 
-app.get("/startTimes", function(req,res){
-	screening.getStartTime(req,res);
+app.get("/startTimes", function (req, res) {
+  screening.getStartTime(req, res);
 });
 
-app.get("/filmScreenings/:filmID", function(req,res){
-	screening.getFilmScreening(req,res);
+app.get("/filmScreenings/:filmID", function (req, res) {
+  screening.getFilmScreening(req, res);
 });
 
-app.post("/checkScreeningAvailability", function(req,res){
-	screening.checkScreeningAvailability(req,res);
+app.post("/checkScreeningAvailability", function (req, res) {
+  screening.checkScreeningAvailability(req, res);
 });
 
-app.post("/checkUpdateScreeningAvailability", function(req,res){
-	screening.checkUpdateScreeningAvailability(req,res);
+app.post("/checkUpdateScreeningAvailability", function (req, res) {
+  screening.checkUpdateScreeningAvailability(req, res);
 });
 
-app.get("/filmRunningTime/:filmID", function(req,res){
-	screening.checkRunningTime(req,res);
+app.get("/filmRunningTime/:filmID", function (req, res) {
+  screening.checkRunningTime(req, res);
 });
 
-app.post("/updateScreening/:screeningID", function(req, res) {
-    screening.updateScreening(req, res);
+app.post("/updateScreening/:screeningID", function (req, res) {
+  screening.updateScreening(req, res);
 });
 
-app.post("/createScreening", function(req,res){
-	screening.createScreening(req,res);
+app.post("/createScreening", function (req, res) {
+  screening.createScreening(req, res);
 });
 
-app.post("/deleteScreening/:screeningID", function(req, res){
-    screening.deleteScreening(req, res);
+app.post("/deleteScreening/:screeningID", function (req, res) {
+  screening.deleteScreening(req, res);
 });
 
-app.post("/seatsRemaining/", function(req, res){
+app.post("/seatsRemaining", function (req, res) {
   screening.updateSeatsRemaining(req, res);
+});
+
+app.post("/screeningDates", function (req, res) {
+  screening.getScreeningDates(req, res);
 });
 
 ////////////////////////////////////////////////////////////
@@ -143,8 +147,8 @@ app.post("/seatsRemaining/", function(req, res){
 
 ////////////////////////////////////////////////////////////
 
-app.get("/ticketTypes", function(req,res){
-	ticketType.getTicketTickets(req,res);
+app.get("/ticketTypes", function (req, res) {
+  ticketType.getTicketTickets(req, res);
 });
 
 
@@ -155,32 +159,32 @@ app.get("/ticketTypes", function(req,res){
 
 ////////////////////////////////////////////////////////////
 
-app.get("/bookings", function(req,res){
-	booking.getBookings(req,res);
+app.get("/bookings", function (req, res) {
+  booking.getBookings(req, res);
 });
 
-app.get("/booking/:BookingID", function(req,res){
-	booking.getBooking(req,res);
+app.get("/booking/:BookingID", function (req, res) {
+  booking.getBooking(req, res);
 });
 
-app.post("/createBooking", function(req,res){
-	booking.createBooking(req,res);
+app.post("/createBooking", function (req, res) {
+  booking.createBooking(req, res);
 });
 
-app.post("/updateBooking/:BookingID", function(req, res){
+app.post("/updateBooking/:BookingID", function (req, res) {
   booking.updateBooking(req, res);
 });
 
-app.post("/deleteBooking/:bookingID", function(req, res){
+app.post("/deleteBooking/:bookingID", function (req, res) {
   booking.deleteBooking(req, res);
 });
 
-app.post("/deleteAllBookings", function(req, res){
+app.post("/deleteAllBookings", function (req, res) {
   booking.deleteAllBookings(req, res);
 });
 
-app.get("/bookedSeats/:ScreeningID", function(req,res){
-	booking.bookedSeats(req,res);
+app.get("/bookedSeats/:ScreeningID", function (req, res) {
+  booking.bookedSeats(req, res);
 });
 
 
@@ -196,32 +200,32 @@ app.get("/bookedSeats/:ScreeningID", function(req,res){
 
 ////////////////////////////////////////////////////////////
 
-app.get("/theatre/:TheatreID", function(req, res){
-    Theatre.getTheatre(req, res);
+app.get("/theatre/:TheatreID", function (req, res) {
+  Theatre.getTheatre(req, res);
 });
 
-app.get("/theatres", function(req, res){
-    Theatre.getTheatres(req, res);
+app.get("/theatres", function (req, res) {
+  Theatre.getTheatres(req, res);
 });
 
-app.post("/updateTheatre/:TheatreID", function(req, res){
-    Theatre.updateTheatre(req, res);
+app.post("/updateTheatre/:TheatreID", function (req, res) {
+  Theatre.updateTheatre(req, res);
 });
 
-app.post("/createTheatre", function(req, res){
-    Theatre.createTheatre(req, res);
+app.post("/createTheatre", function (req, res) {
+  Theatre.createTheatre(req, res);
 });
 
-app.post("/deleteTheatre/:TheatreID", function(req, res){
-    Theatre.deleteTheatre(req, res);
+app.post("/deleteTheatre/:TheatreID", function (req, res) {
+  Theatre.deleteTheatre(req, res);
 });
 
-app.get("/theatreCapacity/:TheatreID", function(req,res){
-    Theatre.getTheatreCapacity(req, res);
+app.get("/theatreCapacity/:TheatreID", function (req, res) {
+  Theatre.getTheatreCapacity(req, res);
 });
 
-app.get("/capacity", function(req,res){
-    Theatre.getCapacity(req, res);
+app.get("/capacity", function (req, res) {
+  Theatre.getCapacity(req, res);
 });
 
 
@@ -234,44 +238,44 @@ app.get("/capacity", function(req,res){
 ////////////////////////////////////////////////////////////
 
 
-app.get("/manager/:managerID", function(req,res){
-	manager.getManager(req,res);
+app.get("/manager/:managerID", function (req, res) {
+  manager.getManager(req, res);
 });
 
-app.get("/managers", function(req,res){
-	manager.getManagers(req,res);
+app.get("/managers", function (req, res) {
+  manager.getManagers(req, res);
 });
 
-app.post("/updateManager/:managerID", function(req, res) {
-    manager.updateManager(req, res);
+app.post("/updateManager/:managerID", function (req, res) {
+  manager.updateManager(req, res);
 });
 
-app.post("/createManager/:name?/:username?/:password?", function(req,res){
-	manager.createManager(req,res);
+app.post("/createManager/:name?/:username?/:password?", function (req, res) {
+  manager.createManager(req, res);
 });
 
-app.post("/deleteManager/:managerID", function(req, res){
-    manager.deleteManager(req, res);
+app.post("/deleteManager/:managerID", function (req, res) {
+  manager.deleteManager(req, res);
 });
 
 ////////////////////////////////////////////////////////////
 
 app.post("/login", function (req, res) {
-    const { email, password, userType } = req.body;
+  const { email, password, userType } = req.body;
 
-    //console.log(req.body); // Just for debugging to see incoming data
+  //console.log(req.body); // Just for debugging to see incoming data
 
-    // Check the userType and call the appropriate login function
-    if (userType === 'admin') {
-        login.loginAdmin(req, res);  // Admin login
-    } else if (userType === 'manager') {
-        login.loginManager(req, res); // Manager login
-    } else {
-        return res.status(400).json({ error: "Invalid user type" });
-    }
+  // Check the userType and call the appropriate login function
+  if (userType === 'admin') {
+    login.loginAdmin(req, res);  // Admin login
+  } else if (userType === 'manager') {
+    login.loginManager(req, res); // Manager login
+  } else {
+    return res.status(400).json({ error: "Invalid user type" });
+  }
 });
 
 
-var myServer = app.listen(3000, function() {
-    console.log("Server listening on port 3000");
-  });
+var myServer = app.listen(3000, function () {
+  console.log("Server listening on port 3000");
+});
