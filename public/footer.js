c3b1118b9aaaa5709723d05ed4d42cfc794477ef
AlbertClarke895 <159160081+AlbertClarke895@users.noreply.github.com>
2025-03-27 18:11:13 +0000

    PutIconFontInAllPages
    used this commit to put the icons in every page to give a way to link the facebook, instagram, twitter

--- public/footer.js
+++ public/footer.js
@@ -5,7 +5,9 @@ function footer() {
       &copy; Applied Computing Group 1
     </div>
     <a href="#" class="fa fa-facebook"></a>
-    <a href="#" class="fa fa-twitter"></a>`;
+    <a href="#" class="fa fa-twitter"></a>
+    <a href="#" class="fa fa-instagram"></a>
+`;
 
   $("#footer").addClass("bg-primary").html(footerOutPut);
 }
