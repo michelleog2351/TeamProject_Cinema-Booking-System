41463cb6bbc828ad2aa10c4f91a7bbaf207aab12
catmw <157846537+catmw@users.noreply.github.com>
2025-03-27 21:18:48 +0000

    sign up box

--- public/footer.js
+++ public/footer.js
@@ -3,7 +3,22 @@ function footer() {
     <hr>
     <div class="text-center text-white py-5"> 
       &copy; Applied Computing Group 1
-    </div>`;
+    </div>;
 
+    <input type="email" id="user_email" placeholder="Enter your email" class="p-1" width: 150px; font-size: 12px;>
+            <button onclick="sendEmail()" class="btn btn-dark mt-5" style="padding: 4px 8px; font-size: 12px;">Sign up</button>
+        </div>`;
   $("#footer").addClass("bg-primary").html(footerOutPut);
+}
+function sendEmail() {
+  let email = document.getElementById("user_email").value;
+  
+  fetch("http://localhost:3000/send-email", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: email })
+  })
+  .then(response => response.text())
+  .then(data => alert(data))
+  .catch(error => alert("Error: " + error));
 }
