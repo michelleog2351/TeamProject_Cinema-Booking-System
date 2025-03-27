9f9d4c7c3d97a3d99929be683c96bdc8c791c736
Michelle Ogunade <132501606+michelleog2351@users.noreply.github.com>
2025-03-27 14:42:18 +0000

    uncomment out the added screenings, remove theatre dropdown menu from screenings

--- public/screening/screening.js
+++ public/screening/screening.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   nav();
   footer();
   getJsonData();
-  theatreDD();
 
   $("#add").append(
     `<button type="button" class="addButton btn btn-primary">Create Screening</button>`
@@ -14,35 +13,14 @@ $(document).ready(function () {
 });
 
 async function getJsonData() {
-  let selectedTheatreID = $("#selectTheatre").val();
-
-  //await $.getJSON(`http://localhost:3000/screenings`, function (data) {
-
-  let data = await $.getJSON(`http://localhost:3000/screenings`);
-  $("#tbody").empty();
-
-  let filteredScreenings;
-  if (selectedTheatreID) {
-    filteredScreenings = data.filter(
-      (value) => value.TheatreID == selectedTheatreID
-    );
-  } else {
-    filteredScreenings = data;
-  }
-
-  // let screeningDate = new Date(value.Date);
-  // let formattedDate = screeningDate.toISOString().split("T")[0];
-
-  // let filmname = await getFilmData(value.FilmID);
-
-  // $(`#tbody`).append(
-  //   `<tr>
-  let rows = await Promise.all(
-    filteredScreenings.map(async (value) => {
-      let formattedDate = new Date(value.Date).toISOString().split("T")[0];
+  await $.getJSON(`http://localhost:3000/screenings`, function (data) {
+    $.each(data, async function (i, value) {
+      let screeningDate = new Date(value.Date);
+      let formattedDate = screeningDate.toISOString().split("T")[0];
       let filmname = await getFilmData(value.FilmID);
 
-      return `<tr>
+      $(`#tbody`).append(
+        `<tr>
 								<td id="startTime${value.StartTime}">${value.StartTime}</td>
 								<td id="date${formattedDate}">${formattedDate}</td>
 								<td id="seatsRemaining${value.SeatsRemaining}">${value.SeatsRemaining}</td>
@@ -50,92 +28,28 @@ async function getJsonData() {
 								<td id="filmID${filmname}">${filmname}</td>
 								<td><button type="button" class="updateButton btn btn-secondary" value="${value.ScreeningID}">Update</button></td>
 								<td><button type="button" class="deleteButton btn btn-danger" value="${value.ScreeningID}">Delete</button></td>
-								</tr>`;
-      //   );
-      // });
-    })
-  );
+								</tr>`
+      );
+    });
 
-  // Append all rows at once
-  $("#tbody").append(rows.join(""));
+    $(document).on("click", ".updateButton", function (e) {
+      let ID = e.target.value;
+      localStorage.setItem("ScreeningID", ID);
+      location.replace("http://localhost:3000/screening/updateScreening.html");
+    });
 
-  $(document).on("click", ".updateButton", function (e) {
-    let ID = e.target.value;
-    localStorage.setItem("ScreeningID", ID);
-    location.replace("http://localhost:3000/screening/updateScreening.html");
-  });
-
-  $(document).on("click", ".deleteButton", function (e) {
-    let ID = e.target.value;
-    $.post(`http://localhost:3000/deleteScreening/${ID}`).done(function () {
-      location.replace("http://localhost:3000/screening/screening.html");
+    $(document).on("click", ".deleteButton", function (e) {
+      let ID = e.target.value;
+      $.post(`http://localhost:3000/deleteScreening/${ID}`).done(function () {
+        location.replace("http://localhost:3000/screening/screening.html");
+      });
     });
   });
-  // });
 }
 
 async function getFilmData(ID) {
   let data = await $.getJSON(`http://localhost:3000/film/${ID}`);
   return data.Name;
-}
-
-function theatreDD() {
-  $.getJSON("http://localhost:3000/theatres", function (data) {
-    data.sort((a, b) => String(a.TheatreID).localeCompare(String(b.TheatreID)));
-
-    let selectTheatre = $("#selectTheatre");
-    let screeningsTbody = $("#tbody");
-
-    selectTheatre.html('<option value="" selected>All Theatres</option>');
-    screeningsTbody.empty(); // Clear any previous screenings
-
-    $.each(data, function (i, theatre) {
-      selectTheatre.append(
-        `<option value="${theatre.TheatreID}">${theatre.TheatreID}</option>`
-      );
-    });
-  });
-
-  $("#selectTheatre").change(async function () {
-    console.log("Dropdown changed:", $(this).val());
-
-    let theatreID = $(this).val();
-    let screeningsTbody = $("#tbody");
-
-    screeningsTbody.empty(); // clear
-    $("#noScreeningsAlert").hide(); // hide
-
-    let data = await $.getJSON("http://localhost:3000/screenings");
-    let filteredScreenings = theatreID
-      ? data.filter((screening) => screening.TheatreID == theatreID)
-      : data;
-    if (filteredScreenings.length === 0) {
-      screeningsTbody.empty();
-
-      // If there are no screenings for the selected theatre
-      $("#noScreeningsAlert").show();
-    } else {
-      let rows = await Promise.all(filteredScreenings.map(createScreeningRow));
-      screeningsTbody.append(rows.join("")); // adds all generated rows to the <tbody>,
-      // .join ensures the rows are added as proper HTML rather than as an array
-    }
-  });
-}
-
-// Helper method to create a row for each screening
-async function createScreeningRow(screening) {
-  let formattedDate = new Date(screening.Date).toISOString().split("T")[0];
-  let filmName = await getFilmData(screening.FilmID);
-
-  return `<tr>
-    <td>${screening.StartTime}</td>
-    <td>${formattedDate}</td>
-    <td>${screening.SeatsRemaining}</td>
-    <td>${screening.TheatreID}</td>
-    <td>${filmName}</td>
-    <td><button type="button" class="updateButton btn btn-secondary" value="${screening.ScreeningID}">Update</button></td>
-    <td><button type="button" class="deleteButton btn btn-danger" value="${screening.ScreeningID}">Delete</button></td>
-  </tr>`;
 }
 
 $(document).ready(function () {
