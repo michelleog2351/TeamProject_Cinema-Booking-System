8be3897603252e0fe573d5d54a0c8b606abb069f
AlbertClarke895 <159160081+AlbertClarke895@users.noreply.github.com>
2025-04-03 17:24:30 +0100

    madeMiddleOfFooterNeat
    cleaned up the middle of the footer to look better overall

--- public/footer.js
+++ public/footer.js
@@ -2,21 +2,24 @@ function footer() {
   var footerOutPut = `
     <hr>
     
-    <div id="email_box" class="justify-content-between align-items-left text-white py-3 px-4 bg-primary" style="padding-left:20%; text-align: left;">
-      <p>Sign up for our Newsletter!</p>
-      <input type="email" id="user_email" placeholder="Enter your email" class="p-1" 
-        style="width: 100%; font-size: 14px;">
-      <button onclick="sendEmail()" class="btn btn-dark" 
-        style="padding: 4px 8px; font-size: 12px;">Sign up</button>
-    </div>
+    <div id="email_box" class="justify-content-between align-items-left text-white py-3 px-4 bg-primary" style="padding-left:20%; text-align: left; display: flex; flex-direction: column; align-items: center;">
+    <p style="text-align: center; font-size: 18px;">Sign up for our Newsletter!</p>
+    <input type="email" id="user_email" placeholder="Enter your email" class="p-1" 
+           style="width: 400px; font-size: 18px; text-align: center; border-radius: 10px;">
+    <button onclick="sendEmail()" class="btn btn-dark" 
+            style="padding: 4px 8px; font-size: 18px; text-align: center; margin-top: 10px;">
+      Sign up
+    </button>
+  </div>
+  
 
     <div class="d-flex justify-content-left text-white py-3 px-4 bg-primary" style="padding-left: 20px;">
-      <a href="#" class="fa fa-facebook"></a>
-      <a href="#" class="fa fa-twitter"></a>
-      <a href="#" class="fa fa-instagram"></a>
+      <a href="https://www.facebook.com/" class="fa fa-facebook"></a>
+      <a href="https://x.com/?lang=en" class="fa fa-twitter"></a>
+      <a href="https://www.instagram.com/" class="fa fa-instagram"></a>
     </div>
 
-    <div  class="copyright d-flex justify-content-center align-items-center text-white py-3 px-4 bg-primary">
+    <div style="font-size: 18px;" class="copyright d-flex justify-content-center align-items-center text-white py-3 px-4 ">
       &copy; Applied Computing Group 1
     </div>
   `;
