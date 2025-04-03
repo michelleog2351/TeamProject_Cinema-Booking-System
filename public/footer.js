6347139e963caf41455c4ead7eb21afbd917d222
AlbertClarke895 <159160081+AlbertClarke895@users.noreply.github.com>
2025-04-03 16:29:20 +0100

    moved@ToTheLeft
    i moved the @ to the left to fill the nav out more and make use of its space

--- public/footer.js
+++ public/footer.js
@@ -1,24 +1,30 @@
 function footer() {
   var footerOutPut = `
     <hr>
+    
+    <div id="email_box" class="justify-content-between align-items-left text-white py-3 px-4 bg-primary" style="padding-left:20%; text-align: left;">
+      <p>Sign up for our Newsletter!</p>
+      <input type="email" id="user_email" placeholder="Enter your email" class="p-1" 
+        style="width: 100%; font-size: 14px;">
+      <button onclick="sendEmail()" class="btn btn-dark" 
+        style="padding: 4px 8px; font-size: 12px;">Sign up</button>
+    </div>
 
-            <div id="email_box" class="justify-content-between align-items-left text-white py-3 px-4 bg-primary" style="padding-left:20%;">
-            <p>Sign up for our Newsletter!</p>
-                <input type="email" id="user_email" placeholder="Enter your email" class="p-1" 
-                    style="width: 100%; font-size: 14px; position: left;">
-                <button onclick="sendEmail()" class="btn btn-dark" 
-                    style="padding: 4px 8px; font-size: 12px;">Sign up</button>
-            </div>
-<div class="d-flex justify-content-between align-items-center text-white py-3 px-4 bg-primary">
+    <div class="d-flex justify-content-left text-white py-3 px-4 bg-primary" style="padding-left: 20px;">
+      <a href="#" class="fa fa-facebook"></a>
+      <a href="#" class="fa fa-twitter"></a>
+      <a href="#" class="fa fa-instagram"></a>
+    </div>
+
+    <div  class="copyright d-flex justify-content-center align-items-center text-white py-3 px-4 bg-primary">
       &copy; Applied Computing Group 1
     </div>
-    <a href="#" class="fa fa-facebook"></a>
-    <a href="#" class="fa fa-twitter"></a>
-    <a href="#" class="fa fa-instagram"></a>
-`;
+  `;
 
   $("#footer").addClass("bg-primary").html(footerOutPut);
 }
+
+
 function sendEmail() {
   let email = document.getElementById("user_email").value;
   
